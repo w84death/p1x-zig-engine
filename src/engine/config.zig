2772b4a447f399a6b9c6bf07fbe79e946883bb8a
Krzysztof Krystian Jankowski <kj@p1x.in>
2026-04-04 22:24:44 +0200

    Update config.zig

--- src/engine/config.zig
+++ src/engine/config.zig
@@ -5,7 +5,7 @@
 // *************************************
 
 pub const CONF = struct {
-    pub const VERSION = "1.0";
+    pub const VERSION = "1.0-RC7";
     pub const THE_NAME = "Borowik";
     pub const TAG_LINE = "Grow something awesome.";
     pub const SCREEN_W = 1200;
